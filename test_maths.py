c252da77ad0258e3ba803d317acb72ad26fdefda
I3UX <matheo.plasz@gmail.com>
2024-06-21 15:56:40 +0200

    Test file corrected

--- test_maths.py
+++ test_maths.py
@@ -1,9 +1,14 @@
+"""Test class for simpleMaths"""
+
 import maths
 
 class TestSimpleMath():
-    def testAdd(self):
-        assert maths.SimpleMath.addition(4,5) ==  9
-        assert maths.SimpleMath.addition(1,1) == 2
-    def testSub(self):
-        assert maths.SimpleMath.substract(5,1) == 4
-        assert maths.SimpleMath.substract(0,1) == -1
+    """Test"""
+    def test_add(self):
+        """"Test Add"""
+        assert maths.SimpleMath().addition(4,5) ==  9
+        assert maths.SimpleMath().addition(1,1) == 2
+    def test_sub(self):
+        """"Test Sub"""
+        assert maths.SimpleMath().substract(5,1) == 4
+        assert maths.SimpleMath().substract(0,1) == -1
